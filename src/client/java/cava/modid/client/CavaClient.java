492c64663c07988063a523fb3204af0ce7e56032
yxw1019 <147071791+yxw1019@users.noreply.github.com>
2026-09-21 17:20:24 +0800

    init

---
+++ src/client/java/cava/modid/client/CavaClient.java
@@ -0,0 +1,10 @@
+package cava.modid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CavaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
